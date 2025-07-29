72f664218fba040e5bc25026806404bfcd682b74
MAW PAING THU <paingmawthu@gmail.com>
2025-07-30 00:08:10 +0900

    Component States အကြောင်းလေ့လာခြင်း - State Data ကို သုံးပြီး Item တွေကို ပြအောင်လုပ်ခြင်း

--- yaycha/src/App.jsx
+++ yaycha/src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
+
 import Item from "./Item";
 import List from "./List";
 
 export default function App() {
+    const [data, setData] = useState([
+        { id: 1, content: "Hello, World!", name: "Alice" },
+        { id: 2, content: "React is fun.", name: "Bob" },
+        { id: 3, content: "Yay, interesting.", name: "Chris" },
+    ])
     return (
         <div style={{maxWidth: 600, margin: "20px auto"}}>
             <h1>Yaycha</h1>
             <List>
-                <Item content="Hello, World!" name="Alice"></Item>
-                <Item content="React is fun." name="Bob"></Item>
+                {
+                    data.map(item => (
+                            <Item key={item.id} content={item.content} name={item.name} />
+                        )
+                    )
+                }
             </List>
         </div>
     )
