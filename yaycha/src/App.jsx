7f81f14d7bb40a898e241e3bae551a1503d57317
MAW PAING THU <paingmawthu@gmail.com>
2025-07-30 00:24:48 +0900

    State Action ကို လေ့လာခြင်း - Delete feature ထည့်လိုက်ပါတယ်

--- yaycha/src/App.jsx
+++ yaycha/src/App.jsx
@@ -9,13 +9,16 @@ export default function App() {
         { id: 2, content: "React is fun.", name: "Bob" },
         { id: 3, content: "Yay, interesting.", name: "Chris" },
     ])
+    const remove = id => {
+        setData(data.filter(item => item.id !== id))
+    }
     return (
         <div style={{maxWidth: 600, margin: "20px auto"}}>
             <h1>Yaycha</h1>
             <List>
                 {
                     data.map(item => (
-                            <Item key={item.id} content={item.content} name={item.name} />
+                            <Item key={item.id} item={item} remove={remove}/>
                         )
                     )
                 }
