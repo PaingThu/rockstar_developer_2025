366e0839df83fb4b42abf7e90325218615dea61a
MAW PAING THU <paingmawthu@gmail.com>
2025-07-30 01:20:14 +0900

    Conditional Rendering အကြောင်းလေ့လာခြင်း

--- yaycha/src/App.jsx
+++ yaycha/src/App.jsx
@@ -10,6 +10,8 @@ export default function App() {
         { id: 2, content: "React is fun.", name: "Bob" },
         { id: 3, content: "Yay, interesting.", name: "Chris" },
     ])
+    // Form ကို ပြ/မပြသတ်မှတ်မဲ့ state variable
+    const [showForm, setShowForm] = useState(false);
     const remove = id => {
         setData(data.filter(item => item.id !== id))
     }
@@ -19,8 +21,27 @@ export default function App() {
     }
     return (
         <div style={{maxWidth: 600, margin: "20px auto"}}>
-            <h1>Yaycha</h1>
-            <Form add={add} />
+            <h1 style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center"
+            }}>
+                Yaycha
+                <button 
+                    onClick={() => setShowForm(!showForm)}
+                    style={{
+                        width: 32,
+                        height: 32,
+                        borderRadius: 50,
+                        border: "0 none",
+                        background: showForm ? "#dc3545" : "#0d6efd",
+                        color: "white"
+                    }}
+                >
+                    {showForm ? "x" : "+"}
+                </button>
+            </h1>
+            { showForm && <Form add={add} /> }
             <List>
                 {
                     data.map(item => (
