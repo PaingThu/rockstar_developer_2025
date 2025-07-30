fe7d674eccc9992d95be859ea1078e7377767ae6
MAW PAING THU <paingmawthu@gmail.com>
2025-07-30 09:51:46 +0900

    React - Context Provider အကြောင်းလေ့လာခြင်း

--- yaycha/src/main.jsx
+++ yaycha/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
+import './index.css'
+import TheamedApp from './ThemedApp.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <TheamedApp />
+    </StrictMode>,
 )
